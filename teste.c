c38f069e1572360b864c21685ce1b99ad0c2b2b3
Welepy-dev <mwsilva.contato@gmail.com>
2024-11-09 15:57:42 +0100

    deleted some files, add test.c

--- teste.c
+++ teste.c
@@ -1,7 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   teste.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcsilv <marcsilv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 11:34:37 by marcsilv          #+#    #+#             */
+/*   Updated: 2024/11/09 15:04:12 by marcsilv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
 #include "ft_printf.h"
+#include <math.h>
 
 int main(void)
 {
-    ft_printf("%#x\n", 255);
+    ft_printf("custom printf: %+d\n", 42);
+    printf("original printf: %+d\n", 42);
     return (0);
 }
