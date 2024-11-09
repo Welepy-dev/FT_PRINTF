236fae3cecff10b750471453a7a91df1cff50fb9
Welepy <149401955+Welepy-dev@users.noreply.github.com>
2024-11-09 19:44:16 +0100

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -22,28 +22,29 @@
 # include <stdarg.h>
 # include <stdbool.h>
 
-int	upper(unsigned int decimalNumber);
-int	lower(unsigned int decimalNumber);
-int	find_specifier(char *string, va_list args);
-int	ft_printf(const char *string, ...);
 int	decimal_to_octal(unsigned int decimal_number);
+int	find_specifier(char *string, va_list args);
 int 	print_hash(char *string, va_list args);
 int	ft_print_ptr(unsigned long long ptr);
+int	ft_printf(const char *string, ...);
+int	upper(unsigned int decimalNumber);
+int	lower(unsigned int decimalNumber);
 int	ft_print_unsigned(unsigned int n);
+int 	put_float(float n, int precision);
 int	ft_num_len(unsigned int num);
 int	ft_ptr_len(uintptr_t num);
 int	ft_putstr(char *str);
 int	ft_putchar(char c);
-int 	put_float(float n, int precision);
 int	ft_putnbr(int n);
+
+bool in_range(int c, int min, int max);
+bool  ft_isdigit(int c);
+
+size_t	ft_strlen(const char *s);
 
 char	*ft_uitoa(unsigned int n);
 
 void	ft_put_ptr(uintptr_t num);
-
-size_t	ft_strlen(const char *s);
-
-bool  ft_isdigit(int c);
 
 size_t num_len(int n);
 
