be0ac3a385c70056cc60239a563b14a7437ea1e3
{Welepy-dev} <mwsilva.contato@gmail.com>
2024-12-13 19:57:17 +0100

    added is_negative function

--- utils/bools.c
+++ utils/bools.c
@@ -6,7 +6,7 @@
 /*   By: marcsilv <marcsilv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 16:04:15 by marcsilv          #+#    #+#             */
-/*   Updated: 2024/12/13 16:04:38 by marcsilv         ###   ########.fr       */
+/*   Updated: 2024/12/13 19:20:19 by marcsilv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,9 @@ bool	ft_isdigit(int c)
 bool	in_range(int c, int min, int max)
 {
 	return (c >= min && c <= max);
+}
+
+bool	is_negative(int n)
+{
+	return (n < 0);
 }
